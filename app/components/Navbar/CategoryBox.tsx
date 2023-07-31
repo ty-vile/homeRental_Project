89385a212ed2a729fc84046ab309835a2b4d9a16
Ty <typhn.sa@gmail.com>
2023-07-31 22:03:45 +0930

    add category params filter

--- app/components/Navbar/CategoryBox.tsx
+++ app/components/Navbar/CategoryBox.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { useRouter, useSearchParams } from "next/navigation";
+import { useCallback } from "react";
 import { IconType } from "react-icons";
+import qs from "query-string";
 
 interface CategoryBoxProps {
   label: string;
@@ -12,8 +15,39 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({
   icon: Icon,
   selected,
 }) => {
+  const router = useRouter();
+  const params = useSearchParams();
+
+  const handleClick = useCallback(() => {
+    let currentQuery = {};
+
+    if (params) {
+      currentQuery = qs.parse(params.toString());
+    }
+
+    const updatedQuery: any = {
+      ...currentQuery,
+      category: label,
+    };
+
+    if (params?.get("category") === label) {
+      delete updatedQuery.category;
+    }
+
+    const url = qs.stringifyUrl(
+      {
+        url: "/",
+        query: updatedQuery,
+      },
+      { skipNull: true }
+    );
+
+    router.push(url);
+  }, [label, params, router]);
+
   return (
     <div
+      onClick={handleClick}
       className={`flex flex-col items-center justify-center gap-2 p-2 border-b-2 transition cursor-pointer hover:text-primary
     ${selected ? "border-b-primary" : "border-transparent"}
     ${selected ? "text-primary" : "text-gray-500"}
