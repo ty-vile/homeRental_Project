4911163123b7cbc933ee52fb969ccf947b684c50
Ty <typhn.sa@gmail.com>
2023-08-02 22:25:28 +0930

    begin listing cards

--- app/components/Listing/ListingCard.tsx
+++ app/components/Listing/ListingCard.tsx
@@ -1,8 +1,47 @@
 "use client";
 
-interface ListingCardProps {}
+import useCountries from "@/app/hooks/useCountries";
+import { Listing, Reservation, User } from "@prisma/client";
+import { useRouter } from "next/navigation";
+import { useCallback } from "react";
 
-const ListingCard = () => {
+interface ListingCardProps {
+  data: Listing;
+  reservation?: Reservation;
+  onAction?: (id: string) => void;
+  disabled?: boolean;
+  actionLabel?: string;
+  actionId: string;
+  currentUser?: User | null;
+}
+
+const ListingCard: React.FC<ListingCardProps> = ({
+  data,
+  reservation,
+  onAction,
+  disabled,
+  actionLabel,
+  actionId,
+  currentUser,
+}) => {
+  const router = useRouter();
+  const { getCountryByValue } = useCountries();
+
+  const location = getCountryByValue(data.locationValue);
+
+  const handleCancel = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.stopPropagation();
+
+      if (disabled) {
+        return;
+      }
+
+      onAction?.(actionId);
+    },
+    [disabled, actionId, onAction]
+  );
+
   return <div>ListingCard</div>;
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,11 @@ import EmptyState from "./components/EmptyState";
 // server-functions
 import getListings from "./actions/getListings";
 import ListingCard from "./components/Listing/ListingCard";
+import getCurrentUser from "./actions/getCurrentUser";
 
 export default async function Home() {
   const listings = await getListings();
+  const currentUser = await getCurrentUser();
 
   if (listings?.length === 0) {
     return <EmptyState showReset />;
@@ -16,7 +18,9 @@ export default async function Home() {
     <Container>
       <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
         {listings?.map((listing, i) => {
-          return <ListingCard key={i} data={listing} />;
+          return (
+            <ListingCard key={i} data={listing} currentUser={currentUser} />
+          );
         })}
       </div>
     </Container>
